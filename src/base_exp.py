8f83cf971097030397a5fb11fb53a9367b49da21
osuossu8 <osuosuossu18@gmail.com>
2022-10-25 22:11:22 +0900

    base_exp

--- src/base_exp.py
+++ src/base_exp.py
@@ -132,48 +132,19 @@ class ForTableDataset(torch.utils.data.Dataset):
 class ForTableModel(nn.Module):
     def __init__(self, CFG):
         super(ForTableModel, self).__init__()
-        self.hidden_size = [1500, 1250, 1000, 750]
-        self.dropout_value = [0.5, 0.35, 0.3, 0.25]
 
-        self.batch_norm1 = nn.BatchNorm1d(len(CFG.numerical_cols))
-        self.dense1 = nn.Linear(len(CFG.numerical_cols), self.hidden_size[0])
-        
-        self.batch_norm2 = nn.BatchNorm1d(self.hidden_size[0])
-        self.dropout2 = nn.Dropout(self.dropout_value[0])
-        self.dense2 = nn.Linear(self.hidden_size[0], self.hidden_size[1])
+        self.regressor = nn.Sequential(
+            nn.Linear(len(CFG.numerical_cols), 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 16),
+            nn.ReLU(inplace=True),
+            nn.Linear(16, CFG.target_size)
+        )
 
-        self.batch_norm3 = nn.BatchNorm1d(self.hidden_size[1])
-        self.dropout3 = nn.Dropout(self.dropout_value[1])
-        self.dense3 = nn.Linear(self.hidden_size[1], self.hidden_size[2])
-
-        self.batch_norm4 = nn.BatchNorm1d(self.hidden_size[2])
-        self.dropout4 = nn.Dropout(self.dropout_value[2])
-        self.dense4 = nn.Linear(self.hidden_size[2], self.hidden_size[3])
-
-        self.batch_norm5 = nn.BatchNorm1d(self.hidden_size[3])
-        self.dropout5 = nn.Dropout(self.dropout_value[3])
-        self.dense5 = nn.utils.weight_norm(nn.Linear(self.hidden_size[3], len(CFG.target_cols)))
-    
-    def forward(self, x):
-        x = self.batch_norm1(x)
-        x = F.leaky_relu(self.dense1(x))
-        
-        x = self.batch_norm2(x)
-        x = self.dropout2(x)
-        x = F.leaky_relu(self.dense2(x))
-
-        x = self.batch_norm3(x)
-        x = self.dropout3(x)
-        x = F.leaky_relu(self.dense3(x))
-
-        x = self.batch_norm4(x)
-        x = self.dropout4(x)
-        x = F.leaky_relu(self.dense4(x))
-
-        x = self.batch_norm5(x)
-        x = self.dropout5(x)
-        x = self.dense5(x)
-        return x
+    def forward(self, features):
+        # bs, len_features
+        output = self.regressor(features)
+        return output
 
 
 def train_one_epoch(model, dataloader, device, epoch, criterion, optimizer, scheduler):
@@ -225,7 +196,7 @@ def train_one_epoch(model, dataloader, device, epoch, criterion, optimizer, sche
 
 
 @torch.no_grad()
-def valid_one_epoch(model, dataloader, device, epoch, criterion):
+def valid_one_epoch(model, dataloader, device, criterion):
     model.eval()
 
     dataset_size = 0
@@ -299,7 +270,7 @@ def train_loop(fold):
         start_time = time.time()
 
         train_epoch_loss = train_one_epoch(model, train_loader, device, epoch, criterion, optimizer, scheduler)
-        valid_epoch_loss, valid_preds = valid_one_epoch(model, valid_loader, device, epoch, criterion)
+        valid_epoch_loss, pred = valid_one_epoch(model, valid_loader, device, criterion)
     
         score = get_score(pred, valid_labels)
 
