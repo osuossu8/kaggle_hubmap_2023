971ca85997b25d3b1f2fcf6d5dff5e270a8ab676
osuossu8 <osuosuossu18@gmail.com>
2022-10-26 14:11:24 +0900

    add Makefile

--- tests/test_split_data.py
+++ tests/test_split_data.py
@@ -13,6 +13,7 @@ class TestDataSplitter:
         self.MultiTargetCFG = BaseCFG(42, 5, 'group', ['y1', 'y2', 'y3'])
 
     def test_split_kfold(self):
+        # Arrange
         input_df = pd.DataFrame()
         input_df['y'] = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
         input_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
@@ -22,11 +23,14 @@ class TestDataSplitter:
         expect_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         expect_df['kfold'] = [0, 2, 2, 4, 3, 1, 4, 3, 1, 0, 3, 0, 4, 1, 2]
         
+        # Act
         actual_df = DataSplitter.split_kfold(input_df, self.CFG)
         
+        # Assert
         assert_frame_equal(expect_df, actual_df)
         
     def test_split_stratified(self):
+        # Arrange
         input_df = pd.DataFrame()
         input_df['y'] = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
         input_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
@@ -36,11 +40,14 @@ class TestDataSplitter:
         expect_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         expect_df['kfold'] = [0, 2, 4, 0, 0, 4, 3, 3, 1, 2, 2, 1, 1, 3, 4]
         
+        # Act
         actual_df = DataSplitter.split_stratified(input_df, self.CFG)
         
+        # Assert
         assert_frame_equal(expect_df, actual_df)
         
     def test_split_group(self):
+        # Arrange
         input_df = pd.DataFrame()
         input_df['group'] = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]
         input_df['y'] = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
@@ -52,11 +59,14 @@ class TestDataSplitter:
         expect_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         expect_df['kfold'] = [4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0]
         
+        # Act
         actual_df = DataSplitter.split_group(input_df, self.CFG)
         
+        # Assert
         assert_frame_equal(expect_df, actual_df)
         
     def test_split_stratified_group(self):
+        # Arrange
         input_df = pd.DataFrame()
         input_df['group'] = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]
         input_df['y'] = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1]
@@ -68,11 +78,14 @@ class TestDataSplitter:
         expect_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         expect_df['kfold'] = [3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 2, 2, 4, 4, 4]
         
+        # Act
         actual_df = DataSplitter.split_stratified_group(input_df, self.CFG)
         
+        # Assert
         assert_frame_equal(expect_df, actual_df)
 
     def test_split_multilabel_stratified(self):
+        # Arrange
         input_df = pd.DataFrame()
         input_df['y1'] = [0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1]
         input_df['y2'] = [0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0]
@@ -86,6 +99,8 @@ class TestDataSplitter:
         expect_df['x'] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         expect_df['kfold'] = [1, 1, 2, 1, 4, 0, 0, 3, 4, 4, 0, 2, 3, 2, 3]
         
+        # Act
         actual_df = DataSplitter.split_multilabel_stratified(input_df, self.MultiTargetCFG)
         
+        # Assert
         assert_frame_equal(expect_df, actual_df)
