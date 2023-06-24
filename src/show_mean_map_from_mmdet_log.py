409033c3771f239fb0881f341b36d772ec1f7854
osuossu8 <osuosuossu18@gmail.com>
2023-06-24 19:16:13 +0900

    show_mean_map_from_mmdet_log

---
+++ src/show_mean_map_from_mmdet_log.py
@@ -0,0 +1,27 @@
+import json
+import glob
+
+import numpy as np
+from typing import Tuple
+
+
+def show_mAP_from_log(log_path: str) -> Tuple[float, str]:
+    log_file = list(open(log_path))
+    last_best_map_str = [ll for ll in log_file if 'best_coco_segm_mAP_epoch' in ll][-1]
+    last_best_map = float(last_best_map_str.split()[11])
+    return last_best_map, last_best_map_str
+
+
+EXP_IDs = ['012', '013', '014', '015', '016']
+
+for EXP_ID in EXP_IDs:
+    exp_maps = []
+    exp_log_paths = glob.glob(f'work_dirs/exp{EXP_ID}/fold*/*/*.log')
+    for log_path in exp_log_paths:
+        last_best_map, last_best_map_str = show_mAP_from_log(log_path) 
+
+        print(EXP_ID, log_path.split('/')[4])
+        print(last_best_map_str)
+        exp_maps.append(last_best_map)
+    print(f'*** mean mAP is {np.mean(exp_maps):4f} at exp{EXP_ID} ***')
+    print()
