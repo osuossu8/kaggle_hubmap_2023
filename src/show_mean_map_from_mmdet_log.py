160514e8112afabd65ea9cac8966a07876bb90e1
osuossu8 <osuosuossu18@gmail.com>
2023-06-24 19:22:37 +0900

    show_mean_map_from_mmdet_log

--- src/show_mean_map_from_mmdet_log.py
+++ src/show_mean_map_from_mmdet_log.py
@@ -12,16 +12,19 @@ def show_mAP_from_log(log_path: str) -> Tuple[float, str]:
     return last_best_map, last_best_map_str
 
 
-EXP_IDs = ['012', '013', '014', '015', '016']
-
-for EXP_ID in EXP_IDs:
+def show_mean_mAP_from_log(EXP_ID: str) -> None:
     exp_maps = []
     exp_log_paths = glob.glob(f'work_dirs/exp{EXP_ID}/fold*/*/*.log')
     for log_path in exp_log_paths:
         last_best_map, last_best_map_str = show_mAP_from_log(log_path) 
-
         print(EXP_ID, log_path.split('/')[4])
         print(last_best_map_str)
         exp_maps.append(last_best_map)
     print(f'*** mean mAP is {np.mean(exp_maps):4f} at exp{EXP_ID} ***')
-    print()
+    print() 
+
+
+EXP_IDs = ['012', '013', '014', '015', '016']
+
+for EXP_ID in EXP_IDs:
+    show_mean_mAP_from_log(EXP_ID)
