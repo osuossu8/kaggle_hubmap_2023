e03efe4213c5ab93ccc355da59a7ecfd4408b515
osuossu8 <osuosuossu18@gmail.com>
2023-06-24 19:28:19 +0900

    add 017, 016 -> (840,840)

--- src/show_mean_map_from_mmdet_log.py
+++ src/show_mean_map_from_mmdet_log.py
@@ -1,4 +1,3 @@
-import json
 import glob
 
 import numpy as np
--- src/train_5fold.py
+++ src/train_5fold.py
@@ -1,10 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
+import glob
 import json
 import logging
 import subprocess
 import os
 import os.path as osp
+import numpy as np
+from typing import Tuple
 
 from mmengine.config import Config, DictAction
 from mmengine.logging import print_log
@@ -59,6 +62,25 @@ def parse_args():
         os.environ['LOCAL_RANK'] = str(args.local_rank)
 
     return args
+
+
+def show_mAP_from_log(log_path: str) -> Tuple[float, str]:
+    log_file = list(open(log_path))
+    last_best_map_str = [ll for ll in log_file if 'best_coco_segm_mAP_epoch' in ll][-1]
+    last_best_map = float(last_best_map_str.split()[11])
+    return last_best_map, last_best_map_str
+
+
+def show_mean_mAP_from_log(EXP_ID: str) -> None:
+    exp_maps = []
+    exp_log_paths = glob.glob(f'work_dirs/exp{EXP_ID}/fold*/*/*.log')
+    for log_path in exp_log_paths:
+        last_best_map, last_best_map_str = show_mAP_from_log(log_path) 
+        print(EXP_ID, log_path.split('/')[4])
+        print(last_best_map_str)
+        exp_maps.append(last_best_map)
+    print(f'*** mean mAP is {np.mean(exp_maps):4f} at exp{EXP_ID} ***')
+    print() 
 
 
 def write_dataset_metadata_json(cfg: Config) -> None:
@@ -160,6 +182,8 @@ def main():
 
         # start training
         runner.train()
+
+    show_mean_mAP_from_log(cfg.EXP_ID)
 
     write_dataset_metadata_json(cfg)
 
