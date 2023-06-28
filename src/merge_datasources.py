cb2a53d9c68f93f2d945ab9d471cb5157ff77e93
osuossu8 <osuosuossu18@gmail.com>
2023-06-29 00:13:59 +0900

    024, ds1+ds2(pl, th08)+ds3(pl, th08), 015 config

--- src/merge_datasources.py
+++ src/merge_datasources.py
@@ -5,11 +5,13 @@ from pathlib import Path
 
 from mmengine.fileio import dump, load
 
-pseudo_threshold = "0-9"  # '0-8'
+EXP_ID = "021"
+pseudo_threshold = '0-8'
 DATA_SOURCE_1 = "/workspace/kaggle_hubmap_2023/input/hubmap-converted-to-coco-ds1-5fold"
 DATA_SOURCE_2 = f"/workspace/kaggle_hubmap_2023/input/hubmap-converted-to-coco-ds2-5fold-pseudo-labeled-{pseudo_threshold}"
+DATA_SOURCE_3 = f"/workspace/kaggle_hubmap_2023/input/hubmap-converted-to-coco-ds3-5fold-pseudo-labeled-{pseudo_threshold}-by-exp{EXP_ID}"
 
-MERGE_DATA_SOURCE_NAME = f"ds1_ds2_th_{pseudo_threshold.replace('-', '_')}"
+MERGE_DATA_SOURCE_NAME = f"ds1_ds2_ds3_th_{pseudo_threshold.replace('-', '_')}"
 for kfold in [0, 1, 2, 3, 4]:
     image_prefix = f"/workspace/kaggle_hubmap_2023/input/{MERGE_DATA_SOURCE_NAME}/fold{kfold}/train"
     out_file = f"/workspace/kaggle_hubmap_2023/input/{MERGE_DATA_SOURCE_NAME}/fold{kfold}/train/annotation_coco.json"
@@ -17,7 +19,8 @@ for kfold in [0, 1, 2, 3, 4]:
 
     source_images1 = glob.glob(f"{DATA_SOURCE_1}/fold{kfold}/train/*.png")
     source_images2 = glob.glob(f"{DATA_SOURCE_2}/fold{kfold}/train/*.png")
-    source_images = source_images1 + source_images2
+    source_images3 = glob.glob(f"{DATA_SOURCE_3}/fold{kfold}/train/*.png")
+    source_images = source_images1 + source_images2 + source_images3
     destination_directory = f"/workspace/kaggle_hubmap_2023/input/{MERGE_DATA_SOURCE_NAME}/fold{kfold}/train/"
 
     for png_path in source_images:
@@ -25,25 +28,41 @@ for kfold in [0, 1, 2, 3, 4]:
 
     anno1 = load(Path(DATA_SOURCE_1) / f"fold{kfold}" / "train/annotation_coco.json")
     anno2 = load(Path(DATA_SOURCE_2) / f"fold{kfold}" / "train/annotation_coco.json")
+    anno3 = load(Path(DATA_SOURCE_3) / f"fold{kfold}" / "train/annotation_coco.json")
 
     new_image = []
     new_image += anno1["images"]
     image_offset = len(anno1["images"])
-    annotation_offset = len(anno1["annotations"])
-
+    image_offset2 = image_offset + len(anno2["images"])
+ 
     for img_dict in anno2["images"]:
         img_dict["id"] += image_offset
+        new_image.append(img_dict)
 
+    print(len(new_image))
+
+    for img_dict in anno3["images"]:
+        img_dict["id"] += image_offset2
         new_image.append(img_dict)
 
     print(len(new_image))
 
     new_anno = []
     new_anno += anno1["annotations"]
+    annotation_offset = len(anno1["annotations"])
+    annotation_offset2 = annotation_offset + len(anno2["annotations"])
 
     for anno_dict in anno2["annotations"]:
         anno_dict["id"] += annotation_offset
         anno_dict["image_id"] += image_offset
+
+        new_anno.append(anno_dict)
+
+    print(len(new_anno))
+
+    for anno_dict in anno3["annotations"]:
+        anno_dict["id"] += annotation_offset2
+        anno_dict["image_id"] += image_offset2
 
         new_anno.append(anno_dict)
 
